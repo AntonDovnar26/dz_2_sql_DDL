36c0360533f2af3bdc536ac03f4efcc4db372109
AntonDovnar26 <leezka21@gmail.com>
2022-04-23 22:06:54 +0300

    1задание

--- practika.sql
+++ practika.sql
@@ -1,1 +1,87 @@
-dscfewtrhertegregmnfvjl/wanfvlwnfhiWEHJNF
+SQL_DDL
+Первая часть.
+
+Таблица employees
+
+Создать таблицу employees
+- id. serial,  primary key,
+- employee_name. Varchar(50), not null
+Наполнить таблицу employee 70 строками.
+
+create table employees (
+id serial primary key,
+employee_name Varchar(50) not null
+);
+
+select * from employees
+
+
+insert into employees (employee_name) values ('Massimo');
+insert into employees (employee_name) values ('Micheal');
+insert into employees (employee_name) values ('Hurleigh');
+insert into employees (employee_name) values ('Andrus');
+insert into employees (employee_name) values ('Florinda');
+insert into employees (employee_name) values ('Malory');
+insert into employees (employee_name) values ('Hanan');
+insert into employees (employee_name) values ('Tarah');
+insert into employees (employee_name) values ('Hieronymus');
+insert into employees (employee_name) values ('Elyssa');
+insert into employees (employee_name) values ('Guthry');
+insert into employees (employee_name) values ('Tiphanie');
+insert into employees (employee_name) values ('Isidore');
+insert into employees (employee_name) values ('Rufus');
+insert into employees (employee_name) values ('Mikey');
+insert into employees (employee_name) values ('Sharyl');
+insert into employees (employee_name) values ('Francisca');
+insert into employees (employee_name) values ('Millard');
+insert into employees (employee_name) values ('Sibella');
+insert into employees (employee_name) values ('Adeline');
+insert into employees (employee_name) values ('Nancee');
+insert into employees (employee_name) values ('Fransisco');
+insert into employees (employee_name) values ('Sofie');
+insert into employees (employee_name) values ('Aguie');
+insert into employees (employee_name) values ('Ernie');
+insert into employees (employee_name) values ('Syman');
+insert into employees (employee_name) values ('Irma');
+insert into employees (employee_name) values ('Henryetta');
+insert into employees (employee_name) values ('Annaliese');
+insert into employees (employee_name) values ('Vladimir');
+insert into employees (employee_name) values ('Flory');
+insert into employees (employee_name) values ('Hersch');
+insert into employees (employee_name) values ('Gloriana');
+insert into employees (employee_name) values ('Lark');
+insert into employees (employee_name) values ('Julina');
+insert into employees (employee_name) values ('Andonis');
+insert into employees (employee_name) values ('Anatole');
+insert into employees (employee_name) values ('Seward');
+insert into employees (employee_name) values ('Neile');
+insert into employees (employee_name) values ('Henriette');
+insert into employees (employee_name) values ('Arlyne');
+insert into employees (employee_name) values ('Blair');
+insert into employees (employee_name) values ('Kristofor');
+insert into employees (employee_name) values ('Letizia');
+insert into employees (employee_name) values ('Olwen');
+insert into employees (employee_name) values ('Annabelle');
+insert into employees (employee_name) values ('Ches');
+insert into employees (employee_name) values ('Josy');
+insert into employees (employee_name) values ('Falito');
+insert into employees (employee_name) values ('Rowney');
+insert into employees (employee_name) values ('Stevie');
+insert into employees (employee_name) values ('Juana');
+insert into employees (employee_name) values ('Alex');
+insert into employees (employee_name) values ('Jodie');
+insert into employees (employee_name) values ('Griffin');
+insert into employees (employee_name) values ('Allison');
+insert into employees (employee_name) values ('Xena');
+insert into employees (employee_name) values ('Kimmi');
+insert into employees (employee_name) values ('Clemmy');
+insert into employees (employee_name) values ('Brynna');
+insert into employees (employee_name) values ('Felicity');
+insert into employees (employee_name) values ('Agata');
+insert into employees (employee_name) values ('Dedra');
+insert into employees (employee_name) values ('Kalle');
+insert into employees (employee_name) values ('Janna');
+insert into employees (employee_name) values ('Pier');
+insert into employees (employee_name) values ('Corbett');
+insert into employees (employee_name) values ('Shel');
+insert into employees (employee_name) values ('Jilleen');
